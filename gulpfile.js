feda24d94adcadd4d6c10ae17708f77ef975e6bd
Ciffelia <mc.prince.0203@gmail.com>
2018-12-18 14:46:58 +0900

    Introduce JavaScript Standard Style

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,28 @@
-const gulp = require('gulp');
-const { htmlmin, plumber, notify, sass } = require('gulp-load-plugins')();
-const browserSync = require('browser-sync');
-const rimraf = require('rimraf');
-const sassPackageImporter = require('node-sass-package-importer');
+const gulp = require('gulp')
+const { htmlmin, plumber, notify, sass } = require('gulp-load-plugins')()
+const browserSync = require('browser-sync')
+const rimraf = require('rimraf')
+const sassPackageImporter = require('node-sass-package-importer')
 
 // Remove dist directory
 const removeDist = callback => {
-  rimraf('dist', callback);
-};
+  rimraf('dist', callback)
+}
 
 // Compile HTML
 const compileHTML = () => {
   return gulp.src('src/html/**/*')
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
     .pipe(htmlmin({ collapseWhitespace: true }))
-    .pipe(gulp.dest('dist'));
-};
+    .pipe(gulp.dest('dist'))
+}
 
 // Copy static files
 const copyStaticFiles = () => {
   return gulp.src('static/**/*')
     .pipe(plumber({ errorHandler: notify.onError('<%= error.message %>') }))
-    .pipe(gulp.dest('dist'));
-};
+    .pipe(gulp.dest('dist'))
+}
 
 // Compile Sass
 const compileSass = () => {
@@ -34,31 +34,31 @@ const compileSass = () => {
       }),
       outputStyle: 'compressed'
     }))
-    .pipe(gulp.dest('dist', { sourcemaps: '.' }));
-};
+    .pipe(gulp.dest('dist', { sourcemaps: '.' }))
+}
 
 // Build
 const build = gulp.series(
   removeDist,
   gulp.parallel(compileHTML, copyStaticFiles, compileSass)
-);
+)
 
 // Watch changes
 const watch = gulp.series(build, async () => {
-  gulp.watch('src/html/**/*', compileHTML);
-  gulp.watch('static/**/*', copyStaticFiles);
-  gulp.watch('src/styles/**/*', compileSass);
-});
+  gulp.watch('src/html/**/*', compileHTML)
+  gulp.watch('static/**/*', copyStaticFiles)
+  gulp.watch('src/styles/**/*', compileSass)
+})
 
 const runBrowserSync = callback => {
-  const bs = browserSync.create();
+  const bs = browserSync.create()
   bs.init({
     server: 'dist',
     files: 'dist'
-  }, callback);
-};
+  }, callback)
+}
 
 // Start development server
-const serve = gulp.series(watch, runBrowserSync);
+const serve = gulp.series(watch, runBrowserSync)
 
-module.exports = { build, watch, serve };
+module.exports = { build, watch, serve }
