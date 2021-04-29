bfc8f1a2b5db1bb7854ebd65ee33ff9ac0a23750
Ciffelia <mc.prince.0203@gmail.com>
2021-04-29 21:42:50 +0900

    Setup styles

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
+import * as styles from './index.module.scss'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <h1>Ciffelia</h1>
+      <div className={styles.main}>Hello world!</div>
+    </>
+  )
 }
