8557997017671c5515f1e334f9afc4a2445c02e5
Ciffelia <mc.prince.0203@gmail.com>
2021-07-14 02:21:28 +0900

    Setup ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  extends: ['standard-with-typescript', 'prettier'],
+  parserOptions: {
+    project: './tsconfig.json'
+  }
+}
