806736eafedf7c9b9a6e47d687b3484ffcd44fa3
Ciffelia <mc.prince.0203@gmail.com>
2022-02-23 23:50:47 +0900

    Introduce emotion

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     'gatsby-plugin-image',
     'gatsby-plugin-sharp',
     'gatsby-plugin-sass',
+    'gatsby-plugin-emotion',
     'gatsby-plugin-typegen',
     'gatsby-transformer-sharp',
     {
