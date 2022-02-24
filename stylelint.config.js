28ed1c34dbd7aa42162cf5fe50fc0837141ee384
Ciffelia <mc.prince.0203@gmail.com>
2022-02-25 00:32:30 +0900

    Fix stylelint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,3 @@
 module.exports = {
-  extends: ['stylelint-config-standard-scss', 'stylelint-config-prettier'],
-  rules: {
-    'color-function-notation': null,
-    'keyframes-name-pattern': null,
-    'selector-class-pattern': null,
-    'shorthand-property-no-redundant-values': null,
-    'scss/dollar-variable-pattern': null
-  }
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier']
 }
