3f0277cb9821d36e3c92265b41d58a7a205a3111
Ciffelia <mc.prince.0203@gmail.com>
2024-01-19 02:11:47 +0900

    introduce stylelint

---
+++ stylelint.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: [
+    "stylelint-config-html",
+    "stylelint-config-standard",
+    "stylelint-config-recess-order",
+  ],
+  rules: {
+    "custom-property-pattern": null,
+  },
+};
